e033df4f26fff07dc18aa9a366e3a4acf01bff32
alexpatoecmf-hub <alexpatoecmf@gmail.com>
2025-08-13 11:27:30 -0300

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1755095444664

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_26481963._.js",
+  "static/chunks/node_modules_9331e0c1._.js"
+],
+    source: "dynamic"
+});
